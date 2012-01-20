b6b22d8a347695c2f3c65d30b80f82adea3a5579
JP Bida <jpbida@JPs-MacBook-Air.local>
2012-01-19 18:33:58 -0800

    Added GPL header

--- apps/pdb2fuzzy.cpp
+++ apps/pdb2fuzzy.cpp
@@ -1,3 +1,18 @@
+/*This file is part of RSIM.
+
+    RSIM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RSIM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with RSIM.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 // Program determines all planar base-pairings that occur in the RNA structure
 // Output File:
 // pdb_id, model, chain, Res_id1, Res_id2, Face1, Face2, hydro
